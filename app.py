4536984d39ca563d5a3a883bec72dcb9ecce8cfb
Oscar Lindqvist <olindqv@gmail.com>
2022-10-31 12:39:53 -0700

    Update csp

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ config.load()
 app = Flask(__name__)
 if env.get("PYTHON_ENV") == "production":
     csp = dict(flask_talisman.GOOGLE_CSP_POLICY)
-    csp["script-src-elem"] = ['\'self\'', 'https://kit.fontawesome.com/*']
+    csp["script-src-elem"] = ['\'self\'', 'kit.fontawesome.com']
     Talisman(app, content_security_policy=csp)
 app.secret_key = env.get("APP_SECRET_KEY")
 
